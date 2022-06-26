5c2b77e77d730388bb74f48aa099a4a37e212380
Tiget1998 <tijs.palings@hotmail.com>
2022-06-26 20:15:09 +0200

    Added code

--- audiofix.py
+++ audiofix.py
@@ -3,4 +3,44 @@ import os
 from pydub import AudioSegment
 
 
-print("Test")
+
+def convert(file):
+    """
+    
+
+    Parameters
+    ----------
+    file : TYPE
+        DESCRIPTION.
+    audformat : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+    audio = AudioSegment.from_file(file)
+    name = file.split("/")[-1]
+    
+    audio.export(directory+"/"+"Music/"+name, format="mp3")
+    
+    return
+
+
+
+if __name__=="__main__":
+    directory = '/'.join(__file__.split('/')[:-1])
+    os.chdir(directory)
+    try:
+        os.mkdir("Music")
+    except FileExistsError:
+        pass
+    path = sys.argv[1]
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if name.endswith(".wma"):
+                print("Converting: "+name)
+                convert(path+'/'+name)
+    print("Finished!")
+    
